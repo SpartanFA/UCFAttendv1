4e5187f677a667202e2d286faed0d692f6c3a4b4
ben <bewerner23@gmail.com>
2023-04-04 22:36:57 -0400

    Got Popup to work

--- app/csvuploader.tsx
+++ app/csvuploader.tsx
@@ -2,48 +2,86 @@
 
 import React, { useState, useRef } from "react";
 import Papa from "papaparse";
-import { Button } from "@tremor/react";
+import { Card, Title, Text, Flex, Button, Grid, Col } from "@tremor/react";
 import { ArrowUpTrayIcon } from '@heroicons/react/24/solid'
+import Popup from 'reactjs-popup'
 
 const Upload = () => {
   const [uploading, setUploading] = useState(false);
   const inputRef = useRef();
 
-  const handleUploadCSV = () => {
+  const handleUploadCSV = (files) => {
     setUploading(true);
 
-    const input = inputRef?.current;
-    const reader = new FileReader();
-    const [file] = input.files;
+    const [file] = files;
 
+    const reader = new FileReader();
     reader.onloadend = ({ target }) => {
       const csv = Papa.parse(target.result, { header: true });
+      // do something with the csv data here
     };
-
     reader.readAsText(file);
   };
 
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.dataTransfer.dropEffect = 'copy';
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    handleUploadCSV(e.dataTransfer.files);
+  };
 
   return (
-    <div>
-      <h4 className="page-header mb-4">Upload a CSV</h4>
       <div className="mb-4">
-        <Button
-          icon={ArrowUpTrayIcon}
-          size="xl"
-          onClick={() => inputRef.current.click()}
-          disabled={uploading}
-          className="btn btn-primary"
-          loading = {uploading}
-        >
-          {uploading ? "Uploading..." : "Import .CSV"}
-        </Button>
-        <input id="file-upload" ref={inputRef} type="file" className="sr-only" disabled={uploading} onChange={handleUploadCSV} />
-      </div>
+        <Popup trigger={
+          <Button
+            icon={ArrowUpTrayIcon}
+            size="xl"
+            className="btn btn-primary"
+          >
+            {uploading ? "Uploading..." : "Import .CSV"}
+          </Button>
+          } 
+          modal
+          nested
 
-    </div>
+        >
+          <Card className="max-w-sm">
+            <Button className = "ImportantButton"
+              size="xl"
+              variant="secondary"
+              color = {uploading ? "neutral" : "blue"}
+              onClick={() => inputRef.current.click()}
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+              loading = {uploading}
+          >
+              {uploading ? "Uploading..." : "Drag and drop .CSV file here"}
+            </Button>
+          <input
+          ref={inputRef}
+          type="file"
+          className="sr-only"
+          disabled={uploading}
+          onChange={(e) => handleUploadCSV(e.target.files)}
+          />
+          </Card>
+        </Popup>
+
+      </div>
   );
 };
+
+const styles = `
+  .ImportantButton {
+    font-size: 24px;
+    padding: 16px;
+  }
+`;
 
 Upload.propTypes = {};
 
